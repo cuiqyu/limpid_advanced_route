12d0f5670b650ba4f88c5c199329fe8593f7b331
cuiqiongyu <cuiqiongyu@unilifemedia.cn>
2020-01-19 17:41:48 +0800

    springcloud使用eureka来实现服务注册中心功能，以及在服务中心注册一个提供服务的eureka_client服务

---
+++ springcloud_learning/eureka_client/src/test/java/com/limpid/eureka_client/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limpid.eureka_client;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
