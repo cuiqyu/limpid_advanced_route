0e86f901359d89758bbbc03302d29135daa2584d
cuiqyu <18720956785@163.com>
2020-02-09 17:22:42 +0800

    添加config_server中git仓库的配置文件

---
+++ springcloud_learning/config_server/src/main/java/com/limpid/config_server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.limpid.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 使用@EnableConfigServer开启配置服务器的功能
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
